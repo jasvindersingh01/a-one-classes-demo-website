3400c5737fbce7bdf9dbce7be5a1482b3f6314c4
Jasvinder Singh <jasvindersingh01@gmail.com>
2025-12-25 18:28:01 +0530

    Fix Mongo error

--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,10 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("MongoDB Error:", err));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
